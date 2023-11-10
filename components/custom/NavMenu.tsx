59f6bed443dd3d4e519d33559b4406a83392be52
lohit <00lohit@gmail.com>
2023-11-10 21:19:12 +0530

    login flow finished

--- components/custom/NavMenu.tsx
+++ components/custom/NavMenu.tsx
@@ -1,6 +1,16 @@
 import { signOut, useSession } from "next-auth/react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useRouter } from "next/navigation";
+import { Button } from "../ui/button";
+
+import { LogOut } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function NavMenu() {
   const router = useRouter();
@@ -10,16 +20,38 @@ export default function NavMenu() {
 
   return (
     <div>
-      <div>
-        <Avatar>
-          <AvatarImage src={image ?? ""} alt="@shadcn" />
-          <AvatarFallback>{name}</AvatarFallback>
-        </Avatar>
-        <p>{name}</p>
+      <div className="p-2 px-4 flex justify-between items-center">
+        <h1 className="font-bold text text-3xl">CertiWiz </h1>
+        <div>
+          {data ? (
+            <>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Avatar>
+                    <AvatarImage src={image ?? ""} alt="@shadcn" />
+                    <AvatarFallback>{name}</AvatarFallback>
+                  </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="mt-2">
+                  <DropdownMenuItem onClick={() => signOut()}>
+                    <LogOut className="mr-2 h-4 w-4" />
+                    <span>Log out</span>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </>
+          ) : (
+            <Button
+              className="my-1"
+              variant={"outline"}
+              onClick={() => router.push("/auth")}
+            >
+              Sign In
+            </Button>
+          )}
+        </div>
       </div>
-      <button onClick={() => signOut()}>Sign Out</button>
-      <button onClick={() => router.push("/auth")}>Sign In</button>
-      <hr className="my-4" />
+      <hr />
     </div>
   );
 }
