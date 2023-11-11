b0797883a047898fa548a1616eac9a9a53f192c4
lohit <00lohit@gmail.com>
2023-11-11 13:35:51 +0530

    search debounce

--- components/NavMenu/Search.tsx
+++ components/NavMenu/Search.tsx
@@ -1,16 +1,26 @@
 import { Input } from "@/components/ui/input";
+import useDebounce from "@/lib/useDebounce";
 import useUrlQuery from "@/lib/useUrlQuery";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export const Search = () => {
   const { value: event } = useUrlQuery("isEvent");
   const { value, genrateUrl } = useUrlQuery("search");
   const router = useRouter();
 
+  const [search, setSearch] = useState(value ?? "");
+  const debouncedValue = useDebounce(search, 690);
+
+  useEffect(() => {
+    router.push(genrateUrl(search));
+    return () => {};
+  }, [debouncedValue]);
+
   const onSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     let value = e.target.value;
     e.preventDefault();
-    value.length > 3 && router.push(genrateUrl(value));
+    setSearch(value);
   };
 
   return (
@@ -18,7 +28,7 @@ export const Search = () => {
       id="search"
       placeholder={!event ? 'Search via "email"...' : 'Search via "event"...'}
       defaultValue={value ?? ""}
-      // value={value ?? ""}
+      value={search}
       onChange={onSearchChange}
       className="flex-1 m-2"
     />
