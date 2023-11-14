92fe7012234f3f51e45898261d3d8fae009f1260
lohit <00lohit@gmail.com>
2023-11-14 12:55:37 +0530

    suspense fix

--- app/events/[id]/page.tsx
+++ app/events/[id]/page.tsx
@@ -1,10 +1,9 @@
-"use server";
 import Editable from "@/components/custom/EventEdit/Editable";
 import { Skeleton } from "@/components/ui/skeleton";
 import { format } from "date-fns";
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 
-export default async function Event({ params }: { params: { id: string } }) {
+export default function Event({ params }: { params: { id: string } }) {
   let { id } = params;
 
   return (
@@ -16,41 +15,49 @@ export default async function Event({ params }: { params: { id: string } }) {
   );
 }
 
+function Sidebar({ id }: { id: string }) {
+  return (
+    <div className={"border-r overflow-hidden flex flex-col relative"}>
+      <Suspense fallback={<Loader />}>
+        <EventData id={id} />
+      </Suspense>
+    </div>
+  );
+}
+
 async function getEvent(id: string) {
-  let res = await fetch(process.env.NEXTAUTH_URL + `/api/events/${id}`);
+  let res = await fetch(`${process.env.NEXTAUTH_URL}/api/events/${id}`);
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
   return res.json();
 }
 
-async function Sidebar({ id }: { id: string }) {
-  let {
-    data: { creator, date, name },
-  } = await getEvent(id);
+const EventData = async ({ id }: { id: string }) => {
+  let { data } = await getEvent(id);
+
+  let { creator, date, name } = data;
 
   let dateText = date && format(new Date(date), "dd-MM-yyyy");
 
   return (
-    <div className={"border-r overflow-hidden flex flex-col relative"}>
-      <Suspense fallback={<Loader />}>
-        <div className="px-3 py-2 mt-4">
-          <p className="text-sm  opacity-50">Event Name</p>
-          <h2 className="mb-2 text-3xl font-semibold">{name}</h2>
-        </div>
-        <div className="px-3 py-2">
-          <p className="text-sm  opacity-50">Creator Name</p>
-          <h2 className="mb-2 text-xl font-medium">{creator}</h2>
-        </div>
-        <div className="px-3 py-2">
-          <p className="text-sm  opacity-50">Date</p>
-          <h2 className="mb-2 text-lg font-medium">{dateText}</h2>
-        </div>
-        <Editable id={id} />
-      </Suspense>
-    </div>
+    <>
+      <div className="px-3 py-2 mt-4">
+        <p className="text-sm  opacity-50">Event Name</p>
+        <h2 className="mb-2 text-3xl font-semibold">{name}</h2>
+      </div>
+      <div className="px-3 py-2">
+        <p className="text-sm  opacity-50">Creator Name</p>
+        <h2 className="mb-2 text-xl font-medium">{creator}</h2>
+      </div>
+      <div className="px-3 py-2">
+        <p className="text-sm  opacity-50">Date</p>
+        <h2 className="mb-2 text-lg font-medium">{dateText}</h2>
+      </div>
+      <Editable id={id} />
+    </>
   );
-}
+};
 
 const Loader = () => (
   <div className="flex flex-col w-full space-y-6 p-3">
